2f21f29ac6f3769d869105177ff0804baa4d7bfe
Kadir <kadir.janaka@consociatesolutions.com>
2026-02-25 11:02:34 +0530

    Add: folder structure

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import http from "http";
+import app from "./app";
+import { connectDB } from "./config/db";
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
