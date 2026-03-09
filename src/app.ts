b423e0fed78c0db912a4b23e4b929049e07b4d4f
Kadir <kadir.janaka@consociatesolutions.com>
2026-03-09 11:21:09 +0530

    Add : remaining controllers

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,12 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes"
 import passport from "./middlewares/passport";
 import googleAuthRoutes from "./routes/oAuth.routes"
+import boardRoutes from "./routes/board.routes";
+import listRoutes from "./routes/list.routes";
+import cardRoutes from "./routes/card.routes";
+import commentRoutes from "./routes/comment.routes";
+import attachmentRoutes from "./routes/attachment.routes";
+
 
 dotenv.config();
 
@@ -16,5 +22,10 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use("/auth", authRoutes);
 app.use("/api/auth", googleAuthRoutes);
+app.use("/api/boards", boardRoutes);
+app.use("/api", listRoutes);
+app.use("/api", cardRoutes);
+app.use("/api", commentRoutes);
+app.use("/api", attachmentRoutes);
 
 export default app;
